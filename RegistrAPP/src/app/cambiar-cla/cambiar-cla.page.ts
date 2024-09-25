5bc7533cecaa5c6cc9a35eb5622dcedc9d872a07
DiegoNoLoler <di.santiagos@duocuc.cl>
2024-09-25 00:19:51 -0300

    cambiarCLave

--- RegistrAPP/src/app/cambiar-cla/cambiar-cla.page.ts
+++ RegistrAPP/src/app/cambiar-cla/cambiar-cla.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cambiar-cla',
@@ -6,10 +7,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cambiar-cla.page.scss'],
 })
 export class CambiarClaPage implements OnInit {
+  claveActual: string;
+  nuevaClave: string;
+  confirmarClave: string;
 
-  constructor() { }
+  constructor(private ToastController: ToastController) {
+    this.claveActual = '';
+    this.nuevaClave = '';
+    this.confirmarClave = '';
+  }
+
 
   ngOnInit() {
+  }
+
+  async cambiarClave() {
+    if (this.claveActual === 'MiClav3') {
+      if (this.nuevaClave === '') {
+        const toast = await this.ToastController.create({
+          message: 'ingresar nueva clave',
+          position: 'middle',
+          duration: 2000,
+          color: 'danger'
+        });
+        toast.present();
+      } else if (this.nuevaClave === this.confirmarClave) {
+        this.claveActual = this.nuevaClave;
+        this.nuevaClave = '';
+        this.confirmarClave = '';
+
+        const toast = await this.ToastController.create({
+          message: 'Clave cambiada correctamente',
+          position: 'middle',
+          duration: 2000,
+          color: 'success'
+        });
+        toast.present();
+      } else {
+        const toast = await this.ToastController.create({
+          message: 'Las claves no coinciden',
+          position: 'middle',
+          duration: 2000,
+          color: 'danger'
+        });
+        toast.present();
+      }
+    } else if (this.claveActual === '') {
+      const toast = await this.ToastController.create({
+        message: 'ingresar clave actual',
+        position: 'middle',
+        duration: 2000,
+        color: 'danger'
+      });
+      toast.present();
+    } else {
+      const toast = await this.ToastController.create({
+        message: 'contraseña incorrecta',
+        position: 'middle',
+        duration: 2000,
+        color: 'danger'
+      });
+      toast.present();
+    }
   }
 
 }
